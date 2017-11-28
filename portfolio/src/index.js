ea6b22eb42f356e158550b0491100da31f8e738f
johanwin <whatsup@johnnguyen.org>
2017-11-27 22:13:57 -0600

    Add babel polypill

--- portfolio/src/index.js
+++ portfolio/src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render } from "react-dom";
 import App from './App';
-// import registerServiceWorker from './registerServiceWorker';
+import 'babel-polyfill';
 
 const renderApp = Component =>
 render(
